4731d604df5588eed3346141b86ace0501d793d3
LuisFSegalla <luisfsegalla@gmail.com>
2024-07-14 13:02:45 -0300

    Adding cpp debug

--- cpp/jogo.cpp
+++ cpp/jogo.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <zmq.hpp>
 #include "jogo.h"
 
 void switchBarcos(int val)
@@ -30,7 +32,12 @@ jogo::jogo()
     tp1 = new tabuleiro;
     tp2 = new tabuleiro;
     round = 1;
-    vidaP1 = vidaP2 = 23;
+    vida = 23;
+    zmq::context_t context(1);
+    socketSend = new zmq::socket_t(context,ZMQ_PUB);
+    socketRecv = new zmq::socket_t(context,ZMQ_SUB);
+    socketSend->bind("tcp://*:5555");
+    socketRecv->connect("tcp://*:1234");
 }
 
 jogo::~jogo()
@@ -100,56 +107,69 @@ void jogo::rodaJogo()
     tabuleiro auxP1;
     tabuleiro auxP2;
     int a = 0,b = 0;
-    while(vidaP1 > 0 || vidaP2 > 0)
+    std::stringstream ss;
+    while (vida < 0)
     {
-        if(round == 1)
-        {
-            std::cout << std::endl << std::endl;
-            auxP1.imprimeTabuleiro();
-            std::cout << "Jogador 1 faça sua jogada: ";
-            std::cin >> a >> b;
-            std::cout << std::endl;
+        ss << "Player 1 with " << vida << " life points.";
+        zmq::message_t request((void*)ss.str().c_str(), ss.str().size()+1, NULL);
+        socketSend->send(request);
+        zmq_sleep(5);
+        zmq::message_t reply;
+        socketRecv->recv(&reply);
+        std::cout << "Received " << reply.size() << " word.\nMessage: " << (const char*)reply.data() << std::endl;
+        vida--;
+    }
+        
+    // while(vidaP1 > 0 || vidaP2 > 0)
+    // {
+    //     if(round == 1)
+    //     {
+    //         std::cout << std::endl << std::endl;
+    //         auxP1.imprimeTabuleiro();
+    //         std::cout << "Jogador 1 faça sua jogada: ";
+    //         std::cin >> a >> b;
+    //         std::cout << std::endl;
 
-            if(auxP1.retPosicao(a,b) != 'X' && tp2->retPosicao(a,b) !=  '*')
-            {
-                auxP1.insereTabuleiro(a,b,'X');
-                vidaP2--;
-            }
-            if(auxP1.retPosicao(a,b) != 'X' && tp2->retPosicao(a,b) ==  '*')
-            {
-                auxP1.insereTabuleiro(a,b,'0');
-            }
-            std::cout << "Tabuleiro do jogador 1 depois da jogada: " << std::endl;
-            auxP1.imprimeTabuleiro();
-            std::cout << std::endl << std::endl;
-            round = 2;
-        }
-        if(round == 2)
-        {
-            std::cout << std::endl << std::endl;
-            auxP2.imprimeTabuleiro();
-            std::cout << "Jogador 2 faça sua jogada: ";
-            std::cin >> a >> b;
-            std::cout << std::endl;
-            if(auxP2.retPosicao(a,b) != 'X' && tp1->retPosicao(a,b) != '*')
-            {
-                auxP2.insereTabuleiro(a,b,'X');
-                vidaP1--;
-            }
-            if(auxP2.retPosicao(a,b) != 'X' && tp1->retPosicao(a,b) == '*')
-            {
-                auxP2.insereTabuleiro(a,b,'0');
-            }
-            std::cout << "Tabuleiro do jogador 2 depois da jogada: " << std::endl;
-            auxP2.imprimeTabuleiro();
-            std::cout << std::endl << std::endl;
-            round = 1;
-        }
-    }
-    if(vidaP1 > vidaP2){
-        std::cout << "Jogador 1 venceu!" << std::endl;
-    }
-    else{
-        std::cout << "Jogador 2 venceu!" << std::endl;
-    }
+    //         if(auxP1.retPosicao(a,b) != 'X' && tp2->retPosicao(a,b) !=  '*')
+    //         {
+    //             auxP1.insereTabuleiro(a,b,'X');
+    //             vidaP2--;
+    //         }
+    //         if(auxP1.retPosicao(a,b) != 'X' && tp2->retPosicao(a,b) ==  '*')
+    //         {
+    //             auxP1.insereTabuleiro(a,b,'0');
+    //         }
+    //         std::cout << "Tabuleiro do jogador 1 depois da jogada: " << std::endl;
+    //         auxP1.imprimeTabuleiro();
+    //         std::cout << std::endl << std::endl;
+    //         round = 2;
+    //     }
+    //     if(round == 2)
+    //     {
+    //         std::cout << std::endl << std::endl;
+    //         auxP2.imprimeTabuleiro();
+    //         std::cout << "Jogador 2 faça sua jogada: ";
+    //         std::cin >> a >> b;
+    //         std::cout << std::endl;
+    //         if(auxP2.retPosicao(a,b) != 'X' && tp1->retPosicao(a,b) != '*')
+    //         {
+    //             auxP2.insereTabuleiro(a,b,'X');
+    //             vidaP1--;
+    //         }
+    //         if(auxP2.retPosicao(a,b) != 'X' && tp1->retPosicao(a,b) == '*')
+    //         {
+    //             auxP2.insereTabuleiro(a,b,'0');
+    //         }
+    //         std::cout << "Tabuleiro do jogador 2 depois da jogada: " << std::endl;
+    //         auxP2.imprimeTabuleiro();
+    //         std::cout << std::endl << std::endl;
+    //         round = 1;
+    //     }
+    // }
+    // if(vidaP1 > vidaP2){
+    //     std::cout << "Jogador 1 venceu!" << std::endl;
+    // }
+    // else{
+    //     std::cout << "Jogador 2 venceu!" << std::endl;
+    // }
 }
--- cpp/jogo.h
+++ cpp/jogo.h
@@ -4,6 +4,7 @@
 #include "tabuleiro.h"
 #include "barco.h"
 #include "minhaMatriz.h"
+#include <zmq.hpp>
 
 class jogo
 {
@@ -11,8 +12,9 @@ private:
     tabuleiro    *tp1;
     tabuleiro    *tp2;
     unsigned int round;
-    unsigned int vidaP1;
-    unsigned int vidaP2;
+    unsigned int vida;
+    zmq::socket_t * socketSend;
+    zmq::socket_t * socketRecv;
 public:
     jogo();
     ~jogo();
